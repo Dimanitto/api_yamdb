67f4477b8ab03c1907cb1836c3b8b628063906b5
Гаврилов Александр <gavrilov-al@mail.ru>
2022-06-09 17:41:25 +0300

    добавил передачу title_id в запрос

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -142,7 +142,6 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         title = get_object_or_404(Title, pk=self.kwargs.get("title_id"))
-
         return title.reviews.all()
 
     def perform_create(self, serializer):
@@ -156,7 +155,9 @@ class CommentViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAdminModeratorOwnerOrReadOnly]
 
     def get_queryset(self):
-        review = get_object_or_404(Review, pk=self.kwargs.get("review_id"))
+        title_id = self.kwargs.get('title_id')
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, pk=review_id, title=title_id)
         return review.comments.all()
 
     def perform_create(self, serializer):
